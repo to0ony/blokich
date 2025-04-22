5610613a131ccf63fa9ceb566376e466e6310f95
to0ony <antonijomaric1@gmail.com>
2025-04-22 23:00:02 +0200

    feat(ui/admin): add navbar to admin dashboard

--- client/blokich/src/app/pages/dashboard-admin/dashboard-admin.component.ts
+++ client/blokich/src/app/pages/dashboard-admin/dashboard-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UploadPanelComponent } from '../../components/admin/upload-panel/upload-panel.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard-admin',
   standalone: true,
-  imports: [UploadPanelComponent],
+  imports: [UploadPanelComponent, NavbarComponent],
   templateUrl: './dashboard-admin.component.html',
   styleUrl: './dashboard-admin.component.scss',
 })
