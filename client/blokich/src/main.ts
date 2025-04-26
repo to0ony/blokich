0858f81325667b6740a585cd014db84ebe0b1ca8
to0ony <antonijomaric1@gmail.com>
2025-04-27 01:09:01 +0200

    feat(client): add vercel analytics integration

--- client/blokich/src/main.ts
+++ client/blokich/src/main.ts
@@ -3,7 +3,10 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { inject } from '@vercel/analytics';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
+
+inject();
 
 bootstrapApplication(AppComponent, {
   providers: [
