de61bdcc72fd8b568881c6fc24ebcbb0d0f2cd08
to0ony <antonijomaric1@gmail.com>
2025-04-27 11:25:31 +0200

    feat(client): add vercel speed insights integration

--- client/blokich/src/main.ts
+++ client/blokich/src/main.ts
@@ -4,9 +4,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 inject();
+injectSpeedInsights();
 
 bootstrapApplication(AppComponent, {
   providers: [
