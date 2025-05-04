d03a2104405d9a8618af2930a92b13259cdc0e35
to0ony <antonijomaric1@gmail.com>
2025-05-04 02:51:27 +0200

    feat(form): implemented new form for searching drivers

---
+++ client/blokich/src/app/components/form/vozac-search-form/vozac-search-form.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { VozaciService } from '../../../services/vozaci.service';
+
+declare var bootstrap: any;
+
+interface VozacInfo {
+  sluz_broj: string;
+  ime_prezime: string;
+}
+
+@Component({
+  standalone: true,
+  selector: 'app-vozac-search-form',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './vozac-search-form.component.html',
+  styleUrl: './vozac-search-form.component.scss',
+})
+export class VozacSearchFormComponent {
+  searchQuery = '';
+  searchResults: VozacInfo[] = [];
+
+  constructor(private vozaciService: VozaciService) {}
+
+  openSearch() {
+    if (!this.searchQuery.trim()) return;
+
+    this.vozaciService.searchVozaci(this.searchQuery).subscribe((res) => {
+      this.searchResults = res;
+
+      const modal = new bootstrap.Modal(document.getElementById('searchModal'));
+      modal.show();
+    });
+  }
+}
