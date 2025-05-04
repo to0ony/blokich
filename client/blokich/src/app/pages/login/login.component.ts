f2103f18f2f8b2d780ce7da2168d83a759f661cd
to0ony <antonijomaric1@gmail.com>
2025-05-04 02:52:07 +0200

    feat(login): included new search driver form in login form

--- client/blokich/src/app/pages/login/login.component.ts
+++ client/blokich/src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from '../../components/form/login-form/login-form.component';
+import { VozacSearchFormComponent } from '../../components/form/vozac-search-form/vozac-search-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, LoginFormComponent],
+  imports: [CommonModule, LoginFormComponent, VozacSearchFormComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
