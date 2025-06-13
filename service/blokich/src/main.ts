296512f186bf3935c266ec429f453dea491fb9e3
to0ony <antonijomaric1@gmail.com>
2025-06-13 19:17:53 +0200

    fix(cors): adjusted cors policies

--- service/blokich/src/main.ts
+++ service/blokich/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       'http://localhost:4200',
       'https://blokich.vercel.app',
       'https://blokich-git-develop-to0onys-projects.vercel.app',
-      'https://blokich-p55l8orrl-to0onys-projects.vercel.app',
+      'https://blokich-git-feature-openholidays-api-in-095a63-to0onys-projects.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
